86709489da021b85b7e0b37770e4332b9c23f872
jhermsmeier <jhermsmeier@googlemail.com>
2013-05-02 21:17:15 +0200

    Added tests

---
+++ test/index.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  
+  'Quoted Printable': {
+    'Encode': require( './qp/encode' ),
+    'Decode': require( './qp/decode' ),
+    'Encode MIME Word': require( './qp/encode_word' ),
+    'Decode MIME Word': require( './qp/decode_word' )
+  }
+  
+}
